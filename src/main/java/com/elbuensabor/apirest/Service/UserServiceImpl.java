138937fa501508194cfe36389dea5813a52d0562
maxidoe2 <maxidoe2004@gmail.com>
2024-09-01 22:35:16 -0300

    changemaxi

--- src/main/java/com/elbuensabor/apirest/Service/UserServiceImpl.java
+++ src/main/java/com/elbuensabor/apirest/Service/UserServiceImpl.java
@@ -22,9 +22,8 @@ public class UserServiceImpl implements UserService{
     @Transactional
     public void createUser(){
         User user = new User("bauti", "1234", "estudiante");
-        System.out.println("Username: " + user.getName());
-        System.out.println("Password: " + user.getPassword());
-        System.out.println("Role: " + user.getRole());
+        User user2 = new User ("maximo","JohnWick2001","estudiante");
         userRepository.save(user);
+        userRepository.save(user2);
     }
 }
